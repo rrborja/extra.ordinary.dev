13a78cc33789ab1bbfada7694933b5eb1e22c18e
Ritchie Borja <code@brute.io>
2020-08-21 20:49:46 -0400

    Add keybase link and improve responsive UX

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,25 +1,32 @@
 import React from 'react'
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
+import versionText from './static-version-text.json';
 import './homepage.css';
 
 const Button = styled.a`
-  /* This renders the buttons above... Edit me! */
   display: inline-block;
   border-radius: 3px;
   padding: 0.5rem 0;
   margin: 0.5rem 1rem;
-  width: 11rem;
-  background: transparent;
+  background: deepskyblue;
   color: white;
-  border: 2px solid white;
-
-  /* The GitHub button is a primary button
-   * edit this to target it specifically! */
-  ${props => props.primary && css`
-    background: white;
-    color: black;
-  `}
+  border: 2px solid deepskyblue;
 `;
+
+const links = Object.freeze([
+  {
+    label: 'GitHub',
+    linkTo: 'https://github.com/rrborja',
+  },
+  {
+    label: 'LinkedIn',
+    linkTo: 'https://www.linkedin.com/in/ritchieborja',
+  },
+  {
+    label: 'Keybase',
+    linkTo: 'https://keybase.io/brute',
+  },
+]);
 
 export default function Homepage() {
   return (
@@ -30,24 +37,24 @@ export default function Homepage() {
       <article>
         <div className="description">
           Hi! I recently got this domain and I am on the verge of finishing this website
-          but for now, I'll just give you a simple one until I make something <a href="https://extra.ordinary.dev"><strong>extraordinary</strong></a>. I am a software engineer in Philadelphia, PA.
-          Feel free to check out the links below.
+          but for now, I'll just give you a simple one until I make something
+          <a href="https://extra.ordinary.dev"> <strong>extraordinary</strong></a>. I am
+          a software engineer in Philadelphia, PA. Feel free to check out the links below.
         </div>
         <div className="social-links">
-          <Button 
-            primary
-            href="https://github.com/rrborja"
-          >
-            GitHub
-          </Button>
-          <Button 
-            primary
-            href="https://www.linkedin.com/in/ritchieborja"
-          >
-            LinkedIn
-          </Button>
+          {links.map(({ label, linkTo }, i) => <Button href={linkTo}>{label}</Button>)}
         </div>
       </article>
+      <footer>
+        <div>
+          {!versionText || versionText.length === 0 ?
+            'If you are reading this, that means CircleCI did not update this line.'
+            : versionText[0]}
+        </div>
+        <div>
+          <a href="https://github.com/rrborja/extra.ordinary.dev">Click here</a> to access the source code of this page.
+        </div>
+      </footer>
     </div>
   );
 }
